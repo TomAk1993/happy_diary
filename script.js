b4f62227603dcbc788b1b4ddd3c8a035cc6e7d3b
TomAk1993 <96138157+TomAk1993@users.noreply.github.com>
2022-06-16 16:31:58 +0100

    innital commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const goodWords = ['happy', 'amazing', 
+'enjoyed', 'fun', 'excited', 'nice', 'calm', 'comfortable', 
+'glad', 'confident'];
+
+const badWords = ['angry', 'sad', 'upset', 'embarrassed', 'nervous', 
+'anxious', 'unhappy', 'miserable', 'worst', 
+'bad'];
+
+const goodColor = 'rgba(0, 255, 255, 0.377)';
+const badColor = 'rgb(255, 0, 0, 0.4)';
+
+const textArea = document.querySelector('.textArea');
+
+textArea.addEventListener('input', function () {
+    // Exit if textarea contains only spaces or empty
+    if (textArea.value.trim().length === 0) {
+        return;
+    }
+
+    // Using regexp break value to array of words
+    const words = textArea.value.match(/\b(\w+)\b/g)
+    let goodCount = 0;
+    let badCount = 0;
+
+    // Cycle through words
+    for (let i = 0; i < words.length; i++) {
+        if (goodWords.includes(words[i])) {
+             goodCount++;
+        }
+
+        if (badWords.includes(words[i])) {
+             badCount++;
+        }
+    }
+
+    if (goodCount > badCount) {
+        document.body.style.backgroundColor = goodColor;
+    } else if (goodCount < badCount) {
+        document.body.style.backgroundColor = badColor;
+    } else if(goodCount === badCount) {
+        document.body.style.backgroundColor = 'white';
+    }
+
+    if(badCount > 0) {
+        const p = document.getElementById('badCountText')
+        p.innerHTML = `word count ${badCount}`;
+        document.append(p);
+    }   else if(goodCount >= 0) {
+        const p = document.getElementById('goodCountText')
+        p.innerHTML = `word count ${goodCount}`;
+        document.append(p);
+    }
+
+})
